d862c484d4274efdb2b0ac8e5051fdb0da9e5034
mitsumisi123 <mitsumisi123@gmail.com>
2022-07-29 16:46:55 +0700

    add test onx

---
+++ test_onx.py
@@ -0,0 +1,21 @@
+
+import numpy as np
+import onnxruntime
+import time
+def main():
+    onnx_path = "model/rotation.onnx"
+
+    x = np.load("test_image/image.npy")
+    print(onnxruntime.get_available_providers())
+    ort_session = onnxruntime.InferenceSession(onnx_path, providers=['CPUExecutionProvider'])
+
+    # compute ONNX Runtime output prediction
+    ort_inputs = {ort_session.get_inputs()[0].name: x}
+    start = time.time()
+    ort_outs = ort_session.run(None, ort_inputs)
+    print("process time:", time.time()-start)
+    print(ort_outs)
+    # compare ONNX Runtime and PyTorch results
+
+if __name__ == "__main__":
+    main()
